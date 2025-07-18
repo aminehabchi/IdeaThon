9df44dd558be116fff4c38fdcf18ac4b232f6c84
aminehabchi <amine.habchi.01@gmail.com>
2025-07-18 23:18:45 +0100

    register endpoint

--- backend/internal/auth/model.go
+++ backend/internal/auth/model.go
@@ -1,1 +1,12 @@
 package auth
+
+type User struct {
+	ID          int    `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	Avatar      string `json:"avatar,omitempty"`
+	PhoneNumber string `json:"phone_number,omitempty"`
+	Bio         string `json:"bio,omitempty"`
+}
