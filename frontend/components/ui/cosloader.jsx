be904fa487f75f66df3fff1bdbdc3f9436c84cf9
BoubkerElmaayouf <boubkerelmaayouf@gmail.com>
2025-07-21 20:38:14 +0100

    adding loader comp and fixing editprofile

---
+++ frontend/components/ui/cosloader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../../app/globals.css"
+
+export const IdeaLoader = () => {
+  return (
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="loader"></div>
+        </div>
+  );
+}
